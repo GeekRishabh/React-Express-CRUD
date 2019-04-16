62a54c2739b737847fac18f22a6dcb3317133a16
Rishabh <rishabh@geekyants.com>
2019-04-16 15:21:10 +0530

    fixed express server

--- Express/dist/config/passport-config.js
+++ Express/dist/config/passport-config.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _passportJwt = require('passport-jwt');
+var _passportJwt = require("passport-jwt");
 
 var _passportJwt2 = _interopRequireDefault(_passportJwt);
 
-var _env = require('./env');
+var _env = require("./env");
 
 var _env2 = _interopRequireDefault(_env);
 
-var _user = require('../server/models/user');
+var _user = require("../server/models/user");
 
 var _user2 = _interopRequireDefault(_user);
 
@@ -20,14 +20,14 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var ExtractJwt = _passportJwt2.default.ExtractJwt;
 var jwtStrategy = _passportJwt2.default.Strategy;
-
 function passportConfiguration(passport) {
   var opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
   // opts.tokenQueryParameterName = ExtractJwt.fromUrlQueryParameter(auth_token);
   opts.secretOrKey = _env2.default.jwtSecret;
   passport.use(new jwtStrategy(opts, function (jwtPayload, cb) {
-    _user2.default.findOneAsync({ _id: jwtPayload._doc._id }).then(function (user) {
+    _user2.default.findOneAsync({ _id: jwtPayload._doc._id }) //eslint-disable-line
+    .then(function (user) {
       return cb(null, user);
     }).error(function (err) {
       return cb(err, false);
@@ -35,5 +35,5 @@ function passportConfiguration(passport) {
   }));
 }
 exports.default = passportConfiguration;
-module.exports = exports['default'];
+module.exports = exports["default"];
 //# sourceMappingURL=passport-config.js.map
--- Express/gulpfile.babel.js
+++ Express/gulpfile.babel.js
@@ -1,53 +1,42 @@
-import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
-import path from 'path';
-import del from 'del';
-import runSequence from 'run-sequence';
+import gulp from "gulp";
+import gulpLoadPlugins from "gulp-load-plugins";
+import path from "path";
+import del from "del";
+import runSequence from "run-sequence";
 // import babelCompiler from 'babel-core/register';
-import * as isparta from 'isparta';
+import * as isparta from "isparta";
 
 const plugins = gulpLoadPlugins();
 
 const paths = {
-  js: ['./**/*.js', '!dist/**', '!node_modules/**', '!coverage/**'],
-  nonJs: ['./package.json', './.gitignore'],
-  tests: './server/tests/*.js'
+  js: ["./**/*.js", "!dist/**", "!node_modules/**", "!coverage/**"],
+  nonJs: ["./package.json", "./.gitignore"],
+  tests: "./server/tests/*.js"
 };
 
-// const options = {
-//   codeCoverage: {
-//     reporters: ['lcov', 'text-summary'],
-//     thresholds: {
-//       global: { statements: 80, branches: 80, functions: 80, lines: 80 }
-//     }
-//   }
-// };
-
-gulp.task("heroku:production", function(){
-    console.log('hello'); // the task does not need to do anything.
+gulp.task("heroku:production", function() {
+  console.log("hello"); // the task does not need to do anything.
 });
 
-
-gulp.task("heroku:development", function(){
-    console.log('hello development'); // the task does not need to do anything.
+gulp.task("heroku:development", function() {
+  console.log("hello development"); // the task does not need to do anything.
 });
 // Clean up dist and coverage directory
-gulp.task('clean', () =>
-  del(['dist/**', 'coverage/**', '!dist', '!coverage'])
-);
+gulp.task("clean", () => del(["dist/**", "coverage/**", "!dist", "!coverage"]));
 
 // Set env variables
-gulp.task('set-env', () => {
+gulp.task("set-env", () => {
   plugins.env({
     vars: {
-      NODE_ENV: 'test'
+      NODE_ENV: "test"
     }
   });
 });
 
 // Lint Javascript
-gulp.task('lint', () =>
-  gulp.src(paths.js)
+gulp.task("lint", () =>
+  gulp
+    .src(paths.js)
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
     .pipe(plugins.eslint())
@@ -60,105 +49,45 @@ gulp.task('lint', () =>
 );
 
 // Copy non-js files to dist
-gulp.task('copy', () =>
-  gulp.src(paths.nonJs)
-    .pipe(plugins.newer('dist'))
-    .pipe(gulp.dest('dist'))
+gulp.task("copy", () =>
+  gulp
+    .src(paths.nonJs)
+    .pipe(plugins.newer("dist"))
+    .pipe(gulp.dest("dist"))
 );
 
 // Compile ES6 to ES5 and copy to dist
-gulp.task('babel', () =>
-  gulp.src([...paths.js, '!gulpfile.babel.js'], { base: '.' })
-    .pipe(plugins.newer('dist'))
+gulp.task("babel", () =>
+  gulp
+    .src([...paths.js, "!gulpfile.babel.js"], { base: "." })
+    .pipe(plugins.newer("dist"))
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.babel())
-    .pipe(plugins.sourcemaps.write('.', {
-      includeContent: false,
-      sourceRoot(file) {
-        return path.relative(file.path, __dirname);
-      }
-    }))
-    .pipe(gulp.dest('dist'))
+    .pipe(
+      plugins.sourcemaps.write(".", {
+        includeContent: false,
+        sourceRoot(file) {
+          return path.relative(file.path, __dirname);
+        }
+      })
+    )
+    .pipe(gulp.dest("dist"))
 );
 
 // Start server with restart on file changes
-gulp.task('nodemon', ['lint', 'copy', 'babel'], () =>
+gulp.task("nodemon", ["copy", "babel"], () =>
   plugins.nodemon({
-    script: path.join('dist', 'index.js'),
-    ext: 'js',
-    ignore: ['node_modules/**/*.js', 'dist/**/*.js'],
-    tasks: ['lint', 'copy', 'babel']
+    script: path.join("dist", "index.js"),
+    ext: "js",
+    ignore: ["node_modules/**/*.js", "dist/**/*.js"],
+    tasks: ["copy", "babel"]
   })
 );
 
-// covers files for code coverage
-gulp.task('pre-test', () =>
-  gulp.src([...paths.js, '!gulpfile.babel.js'])
-    // Covering files
-    .pipe(plugins.istanbul({
-      instrumenter: isparta.Instrumenter,
-      includeUntested: true
-    }))
-    // Force `require` to return covered files
-    .pipe(plugins.istanbul.hookRequire())
-);
-
-// triggers mocha test with code coverage
-gulp.task('test', ['pre-test', 'set-env'], () => {
-  // let reporters;
-  const	exitCode = 0;
-
-  // if (plugins.util.env['code-coverage-reporter']) {
-  //   reporters = [...options.codeCoverage.reporters, plugins.util.env['code-coverage-reporter']];
-  // } else {
-  //   reporters = options.codeCoverage.reporters;
-  // }
-  //
-  // return gulp.src([paths.tests], { read: false })
-  //   .pipe(plugins.plumber())
-  //   .pipe(plugins.mocha({
-  //     reporter: plugins.util.env['mocha-reporter'] || 'spec',
-  //     ui: 'bdd',
-  //     timeout: 6000,
-  //     compilers: {
-  //       js: babelCompiler
-  //     }
-  //   }))
-  //   .once('error', (err) => {
-  //     plugins.util.log(err);
-  //     exitCode = 1;
-  //   })
-  //   // Creating the reports after execution of test cases
-  //   .pipe(plugins.istanbul.writeReports({
-  //     dir: './coverage',
-  //     reporters
-  //   }))
-  //   // Enforce test coverage
-  //   .pipe(plugins.istanbul.enforceThresholds({
-  //     thresholds: options.codeCoverage.thresholds
-  //   }))
-  //   .once('end', () => {
-  //     plugins.util.log('completed !!');
-  //     process.exit(exitCode);
-  //   });
-
-  process.exit(exitCode);
-});
-
-// clean dist, compile js files, copy non-js files and execute tests
-gulp.task('mocha', ['clean'], () => {
-  runSequence(
-    ['copy', 'babel'],
-    'test'
-  );
-});
-
 // gulp serve for development
-gulp.task('serve', ['clean'], () => runSequence('nodemon'));
+gulp.task("serve", ["clean"], () => runSequence("nodemon"));
 
 // default task: clean dist, compile js files and copy non-js files.
-gulp.task('default', ['clean'], () => {
-  runSequence(
-    ['copy', 'babel']
-  );
+gulp.task("default", ["clean"], () => {
+  runSequence(["copy", "babel"]);
 });
